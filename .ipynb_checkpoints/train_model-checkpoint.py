26a5b66ce0f5a6b0cca0a097a649dbc96bef8501
Luke Wilson <lukeduanewilson@gmail.com>
2024-02-26 22:33:45 -0700

    commit before run the ml pipeline

--- .ipynb_checkpoints/train_model-checkpoint.py
+++ .ipynb_checkpoints/train_model-checkpoint.py
@@ -13,14 +13,14 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "Your path here"
+project_path = "./"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = # your code here
+data = pd.read_csv(data_path)
 
 # TODO: split the provided data to have a train dataset and a test dataset
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = # Your code here
+
+train, test = train_test_split(data, test_size=0.2, random_state=42)
 
 # DO NOT MODIFY
 cat_features = [
@@ -36,10 +36,10 @@ cat_features = [
 
 # TODO: use the process_data function provided to process the data.
 X_train, y_train, encoder, lb = process_data(
-    # your code here
-    # use the train dataset 
-    # use training=True
-    # do not need to pass encoder and lb as input
+    data = train,
+    categorical_features = cat_features,
+    training=True,
+    label = "salary"
     )
 
 X_test, y_test, _, _ = process_data(
@@ -52,7 +52,7 @@ X_test, y_test, _, _ = process_data(
 )
 
 # TODO: use the train_model function to train the model on the training dataset
-model = # your code here
+model = train_model(X_train, y_train)
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
@@ -65,8 +65,7 @@ model = load_model(
     model_path
 ) 
 
-# TODO: use the inference function to run the model inferences on the test dataset.
-preds = # your code here
+preds = inference(model, X_test)
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
@@ -79,9 +78,16 @@ for col in cat_features:
     for slicevalue in sorted(test[col].unique()):
         count = test[test[col] == slicevalue].shape[0]
         p, r, fb = performance_on_categorical_slice(
-            # your code here
+            data = test,
+            column_name = col,
+            slice_value = slice_value
+            categorical_features = cat_features,
+            label = "salary",
+            encoder = encoder,
+            lb = lb,
+            model = model,
             # use test, col and slicevalue as part of the input
         )
         with open("slice_output.txt", "a") as f:
-        print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
-        print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
+            print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
+            print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
--- ml/model.py
+++ ml/model.py
@@ -1,8 +1,10 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+
 from ml.data import process_data
 # TODO: add necessary import
-
+from sklearn.svm import SVC
+#from sklearn.tree import RandomForestClassifier - decided against this
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
     """
@@ -20,6 +22,13 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
    # TODO: implement the function
+   # Chose RBF kernel because it's used commonly and it's similar to gaussian. The link below helped me understand the usage much better
+   #https://towardsdatascience.com/radial-basis-function-rbf-kernel-the-go-to-kernel-acf0d22c798a
+
+    svc_model = SVC(kernel='rbf')
+    svc_model.fit(X_train, y_train)
+    return svc_model
+    
     pass
 
 
@@ -59,7 +68,9 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
+    preds = model.predict(X)
+    return preds
+    
     pass
 
 def save_model(model, path):
@@ -72,12 +83,19 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
+
+
+    with open(path, 'wb') as wb:
+        pickle.dump(model, wb)
     pass
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
+    #write mode    
+    with open(path, 'wb') as wb:
+        model = pickle.load(wb)
+    return model
     pass
 
 
@@ -119,10 +137,20 @@ def performance_on_categorical_slice(
     """
     # TODO: implement the function
     X_slice, y_slice, _, _ = process_data(
-        # your code here
+        data = data[data[column_name] == slice_value],
+        categorical_features = categorical_features,
+        label = label,
+        #using training = False
+        training=False,
+        encoder = encoder,
+        lb = lb
+        
+        
+        
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
     )
-    preds = # your code here to get prediction on X_slice using the inference function
+    # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice) 
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
